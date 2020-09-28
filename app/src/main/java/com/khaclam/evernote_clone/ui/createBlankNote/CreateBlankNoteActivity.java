efd7f59d5249c374a6ca2544a40506b395e5bd37
Nguyen Khac Lam <khaclam2409@gmail.com>
2020-09-28 17:04:03 +0700

    Done editor

--- app/src/main/java/com/khaclam/evernote_clone/ui/createBlankNote/CreateBlankNoteActivity.java
+++ app/src/main/java/com/khaclam/evernote_clone/ui/createBlankNote/CreateBlankNoteActivity.java
@@ -1,116 +1,168 @@
 package com.khaclam.evernote_clone.ui.createBlankNote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.chinalwb.are.AREditText;
+import com.chinalwb.are.AREditor;
+import com.chinalwb.are.styles.toolbar.ARE_ToolbarDefault;
+import com.chinalwb.are.styles.toolbar.IARE_Toolbar;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_AlignmentCenter;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_AlignmentLeft;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_AlignmentRight;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_At;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Bold;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Hr;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Image;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Italic;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Link;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_ListBullet;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_ListNumber;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Quote;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Strikethrough;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Subscript;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Superscript;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Underline;
+import com.chinalwb.are.styles.toolitems.ARE_ToolItem_Video;
+import com.chinalwb.are.styles.toolitems.IARE_ToolItem;
 import com.khaclam.evernote_clone.R;
 
-import net.dankito.richtexteditor.android.RichTextEditor;
-import net.dankito.richtexteditor.android.toolbar.GroupedCommandsEditorToolbar;
-import net.dankito.richtexteditor.callback.DidHtmlChangeListener;
-import net.dankito.richtexteditor.callback.GetCurrentHtmlCallback;
-import net.dankito.richtexteditor.callback.HtmlChangedListener;
-import net.dankito.utils.android.permissions.IPermissionsService;
-import net.dankito.utils.android.permissions.PermissionsService;
-
-import org.jetbrains.annotations.NotNull;
+import java.util.Objects;
 
 public class CreateBlankNoteActivity extends AppCompatActivity {
-    private RichTextEditor editor;
-    private GroupedCommandsEditorToolbar bottomGroupedCommandsToolbar;
-    private IPermissionsService permissionsService = new PermissionsService(this);
+    private IARE_Toolbar mToolbar;
+    private AREditText mEditText;
+    private boolean scrollerAtEnd;
+    private EditText edTitle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_blank_note);
 
-        initViews();
+        initsView();
     }
 
-    private void initViews() {
-        editor = (RichTextEditor) findViewById(R.id.editor);
-
-        // this is needed if you like to insert images so that the user gets asked for permission to access external storage if needed
-        // see also onRequestPermissionsResult() below
-        editor.setPermissionsService(permissionsService);
-
-        bottomGroupedCommandsToolbar = (GroupedCommandsEditorToolbar) findViewById(R.id.editorToolbar);
-        bottomGroupedCommandsToolbar.setEditor(editor);
-
-        // you can adjust predefined toolbars by removing single commands
-//        bottomGroupedCommandsToolbar.removeCommandFromGroupedCommandsView(CommandName.TOGGLE_GROUPED_TEXT_STYLES_COMMANDS_VIEW, CommandName.BOLD);
-//        bottomGroupedCommandsToolbar.removeSearchView();
-
-
-        editor.setEditorFontSize(20);
-        editor.setPadding((4 * (int) getResources().getDisplayMetrics().density));
-
-        // some properties you also can set on editor
-//        editor.setEditorBackgroundColor(Color.YELLOW)
-//        editor.setEditorFontColor(Color.MAGENTA)
-//        editor.setEditorFontFamily("cursive")
-
-        // show keyboard right at start up
-//        editor.focusEditorAndShowKeyboardDelayed()
-
-        // only needed if you allow to automatically download remote images
-//        editor.setDownloadImageConfig(new DownloadImageConfig(DownloadImageUiSetting.AllowSelectDownloadFolderInCode,
-//                new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "downloaded_images")));
-
-
-        /*      Set listeners on RichTextEditor         */
-
-        // get informed when edited HTML changed
-        editor.addDidHtmlChangeListener(new DidHtmlChangeListener() {
-            @Override
-            public void didHtmlChange(boolean didHtmlChange) {
-                // e.g. set save button to enabled / disabled
-                // btnSave.setEnabled(didHtmlChange);
-            }
-        });
-
-        // use this listener with care, it may decreases performance tremendously
-        editor.addHtmlChangedListener(new HtmlChangedListener() {
-            @Override
-            public void htmlChangedAsync(@NotNull String html) {
-                // htmlChangedAsync() gets called on a background thread, so if you want to use it on UI thread you have to call runOnUiThread()
-            }
-        });
+    private void initsView() {
+        initToolbar();
+        toolbar = findViewById(R.id.mainToolbar);
+        edTitle = findViewById(R.id.edTitle);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 
-    // Important: Overwrite onBackPressed and pass it to toolbar.There's no other way that it can get informed of back button presses.
     @Override
-    public void onBackPressed() {
-        if(bottomGroupedCommandsToolbar.handlesBackButtonPress() == false) {
-            super.onBackPressed();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.create_text_menu, menu);
+        return true;
+    }
+
+    private void initToolbar() {
+        mToolbar = this.findViewById(R.id.areToolbar);
+        IARE_ToolItem bold = new ARE_ToolItem_Bold();
+        IARE_ToolItem italic = new ARE_ToolItem_Italic();
+        IARE_ToolItem underline = new ARE_ToolItem_Underline();
+        IARE_ToolItem strikethrough = new ARE_ToolItem_Strikethrough();
+        IARE_ToolItem quote = new ARE_ToolItem_Quote();
+        IARE_ToolItem listNumber = new ARE_ToolItem_ListNumber();
+        IARE_ToolItem listBullet = new ARE_ToolItem_ListBullet();
+        IARE_ToolItem hr = new ARE_ToolItem_Hr();
+        IARE_ToolItem link = new ARE_ToolItem_Link();
+        IARE_ToolItem subscript = new ARE_ToolItem_Subscript();
+        IARE_ToolItem superscript = new ARE_ToolItem_Superscript();
+        IARE_ToolItem left = new ARE_ToolItem_AlignmentLeft();
+        IARE_ToolItem center = new ARE_ToolItem_AlignmentCenter();
+        IARE_ToolItem right = new ARE_ToolItem_AlignmentRight();
+        IARE_ToolItem image = new ARE_ToolItem_Image();
+        IARE_ToolItem video = new ARE_ToolItem_Video();
+        IARE_ToolItem at = new ARE_ToolItem_At();
+        mToolbar.addToolbarItem(bold);
+        mToolbar.addToolbarItem(italic);
+        mToolbar.addToolbarItem(underline);
+        mToolbar.addToolbarItem(strikethrough);
+        mToolbar.addToolbarItem(quote);
+        mToolbar.addToolbarItem(listNumber);
+        mToolbar.addToolbarItem(listBullet);
+        mToolbar.addToolbarItem(hr);
+        mToolbar.addToolbarItem(link);
+        mToolbar.addToolbarItem(subscript);
+        mToolbar.addToolbarItem(superscript);
+        mToolbar.addToolbarItem(left);
+        mToolbar.addToolbarItem(center);
+        mToolbar.addToolbarItem(right);
+        mToolbar.addToolbarItem(at);
+
+        mEditText = this.findViewById(R.id.arEditText);
+        mEditText.setToolbar(mToolbar);
+
+
+        initToolbarArrow();
+    }
+
+    private void initToolbarArrow() {
+        final ImageView imageView = this.findViewById(R.id.arrow);
+        if (this.mToolbar instanceof ARE_ToolbarDefault) {
+            ((ARE_ToolbarDefault) mToolbar).getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+                @Override
+                public void onScrollChanged() {
+                    int scrollX = ((ARE_ToolbarDefault) mToolbar).getScrollX();
+                    int scrollWidth = ((ARE_ToolbarDefault) mToolbar).getWidth();
+                    int fullWidth = ((ARE_ToolbarDefault) mToolbar).getChildAt(0).getWidth();
+
+                    if (scrollX + scrollWidth < fullWidth) {
+                        imageView.setImageResource(R.drawable.ic_baseline_arrow_back_24);
+                        scrollerAtEnd = false;
+                    } else {
+                        imageView.setImageResource(R.drawable.ic_baseline_arrow_forward_24);
+                        scrollerAtEnd = true;
+                    }
+                }
+            });
+        }
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (scrollerAtEnd) {
+                    ((ARE_ToolbarDefault) mToolbar).smoothScrollBy(-Integer.MAX_VALUE, 0);
+                    scrollerAtEnd = false;
+                } else {
+                    int hsWidth = ((ARE_ToolbarDefault) mToolbar).getChildAt(0).getWidth();
+                    ((ARE_ToolbarDefault) mToolbar).smoothScrollBy(hsWidth, 0);
+                    scrollerAtEnd = true;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            case R.id.saveBtn:
+                Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show();
+                Log.d("html", "onOptionsItemSelected: " + edTitle.getText().toString() + "---" + mEditText.getHtml());
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
-
-    // only needed if you like to insert images from local device so the user gets asked for permission to access external storage if needed
-
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        permissionsService.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//    }
-
-
-    // then when you want to do something with edited html
-    private void save() {
-        editor.getCurrentHtmlAsync(new GetCurrentHtmlCallback() {
-
-            @Override
-            public void htmlRetrieved(@NotNull String html) {
-                saveHtml(html);
-            }
-        });
-    }
-
-    private void saveHtml(String html) {
-        // ...
-    }
-
 }
